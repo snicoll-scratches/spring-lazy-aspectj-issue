e89bb8fbe91f493b53a78b5c3e7ed132b363e480
Stéphane Nicoll <snicoll@vmware.com>
2023-09-21 15:52:32 +0200

    Add LazyInitializationExcludeFilter for TAS

--- src/main/java/com/example/springlazyaspectjissue/SpringLazyAspectjIssueApplication.java
+++ src/main/java/com/example/springlazyaspectjissue/SpringLazyAspectjIssueApplication.java
@@ -1,9 +1,12 @@
 package com.example.springlazyaspectjissue;
 
+import org.springframework.boot.LazyInitializationExcludeFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AdviceMode;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
 @SpringBootApplication
 @EnableTransactionManagement(mode = AdviceMode.ASPECTJ)
@@ -12,4 +15,9 @@ public class SpringLazyAspectjIssueApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringLazyAspectjIssueApplication.class);
     }
+
+	@Bean
+	public static LazyInitializationExcludeFilter tas() {
+		return LazyInitializationExcludeFilter.forBeanTypes(TransactionAspectSupport.class);
+	}
 }
